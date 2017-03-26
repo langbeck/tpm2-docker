8b32f4b6d6275d5c341618671a7adb5ca0e6d789
Dórian Langbeck <langbeck@hpe.com>
2017-03-25 21:53:48 -0300

    Server mode support for ACS

--- ibmacs/bootstrap.sh
+++ ibmacs/bootstrap.sh
@@ -1,13 +1,37 @@
 #!/bin/bash
 set -e
 
-service apache2 start > /dev/null
-service mysql start > /dev/null
+stdbuf -oL -eL tpm_server 2>&1 1> /tpm_server.log &
+sleep 0.1
 
+powerup
+startup
+
+
+cd acs
 
 if [ "$1" == "--test" ]; then
 	# No tests for ACS for now
 	exit 0
+elif [ "$1" == "--server" ]; then
+	# Server startup
+	service apache2 start > /dev/null 2> /dev/null &
+	service mysql start > /dev/null &
+
+	export ACS_PORT=2323
+	ROOTCERTS=$(mktemp)
+	find /tpm2/utils/certificates/ -name '*.pem' > $ROOTCERTS
+	server -v -root "${ROOTCERTS}"
+	exit $?
 fi
 
+
+exec bash
+
+# Client startup
+openssl genrsa -out cakey.pem -aes256 -passout pass:rrrr 2048 2> /dev/null
+openssl req -new -x509 -key cakey.pem -out cacert.pem -days 3560 -passin pass:rrrr -subj "/C=US/ST=NY/L=Yorktown/O=IBM/CN=EK CA"
+clientek -v
+
+clientenroll -v -ho 172.17.0.2
 exec bash
